9fdfd74bc584c23d141d6a4c482d4241d4a03007
Muhammed <mselrakabawi@gmail.com>
2018-04-17 13:39:39 +0200

    FIB: Rabbits and Recurrence Relations

---
+++ [4]FIB/fib.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use Memoize;
+
+
+memoize('rec_rel');
+
+
+# Compute Recurrence relation sequence (returning n-th element)
+sub rec_rel {
+  my $n = shift;
+  my $k = shift;
+  return $n if $n < 2;
+  rec_rel($n-1, $k) + (rec_rel($n-2, $k) * ($k)); # -> (Fn = Fn-1 + Fn-2(k))
+}
+
+chomp(my $line = <>);
+#n -> Number of generations (n-th el in seq), k -> number of rabbit pairs per generation
+my ($n, $k) = split " ", $line;
+
+print rec_rel($n, $k) . " ";
