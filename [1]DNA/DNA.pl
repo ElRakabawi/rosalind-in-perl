0fb4f1437cae7b0358dee51040b74396d8c76ae0
Muhammed <mselrakabawi@gmail.com>
2018-04-17 10:31:17 +0200

    DNA: Counting DNA Nucleotides

---
+++ [1]DNA/DNA.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+
+my $seq = <STDIN>;
+chomp $seq;
+
+my $seq_len = length($seq);
+my $char;
+
+my @count = (0,0,0,0);
+
+for(my $i=0; $i<$seq_len; $i++) {
+	$char = substr($seq, $i, 1);
+	if($char eq 'A'){
+		$count[0]++;
+	}
+	elsif($char eq 'C') {
+		$count[1]++;
+	}
+	elsif($char eq 'G') {
+		$count[2]++;
+	}
+	elsif($char eq 'T') {
+		$count[3]++;
+	}
+}
+
+print "@count";
